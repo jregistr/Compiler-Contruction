12cf7972c9c505d0979c252acb2c5aae3b1f6e75
Jeff Registre <jeffreyregist365@gmail.com>
2016-12-12 22:13:59 -0500

    p2

---
+++ phase-2/src/main/scala/com/jeff/compiler/Main.scala
@@ -0,0 +1,32 @@
+package com.jeff.compiler
+
+import com.compiler.generated.antlr.{MiniJavaLexer, MiniJavaParser}
+import com.jeff.compiler.error.ParseErrorListener
+import org.antlr.v4.runtime.atn.PredictionMode
+import org.antlr.v4.runtime.tree.ParseTree
+import org.antlr.v4.runtime.{ANTLRInputStream, CommonTokenStream, DiagnosticErrorListener}
+
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+
+    val inputStream = getClass.getClassLoader.getResourceAsStream("HelloWorld.minijava")
+    val antlrStream = new ANTLRInputStream(inputStream)
+
+    val lexer = new MiniJavaLexer(antlrStream)
+    val tokenStream = new CommonTokenStream(lexer)
+    val parser = new MiniJavaParser(tokenStream)
+
+    parser.removeErrorListeners()
+
+    parser.addErrorListener(new DiagnosticErrorListener())
+    parser.getInterpreter.setPredictionMode(PredictionMode.LL_EXACT_AMBIG_DETECTION)
+
+    parser.addErrorListener(new ParseErrorListener)
+
+    val tree:ParseTree = parser.goal()
+
+  }
+
+}
