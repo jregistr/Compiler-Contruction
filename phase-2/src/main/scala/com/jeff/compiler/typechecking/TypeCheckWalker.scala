38ff89e20cc855fb4c247c84159dd09677a5910d
Jeff Registre <jeffreyregist365@gmail.com>
2016-12-15 04:31:27 -0500

    Nearly done with typecheck walker

--- phase-2/src/main/scala/com/jeff/compiler/typechecking/TypeCheckWalker.scala
+++ phase-2/src/main/scala/com/jeff/compiler/typechecking/TypeCheckWalker.scala
@@ -3,7 +3,7 @@ package com.jeff.compiler.typechecking
 import com.compiler.generated.antlr.MiniJavaBaseVisitor
 import com.compiler.generated.antlr.MiniJavaParser._
 import com.jeff.compiler.errorhandling.Errors
-import com.jeff.compiler.typechecking.definitions.{Klass, Scope}
+import com.jeff.compiler.typechecking.definitions._
 import com.jeff.compiler.util.Aliases.ClassMap
 import org.antlr.v4.runtime.ParserRuleContext
 import org.antlr.v4.runtime.tree.ParseTreeProperty
@@ -14,6 +14,10 @@ class TypeCheckWalker(classes:ClassMap, scopes:ParseTreeProperty[Scope]) extends
 
   private var currentScope: Option[Scope] = None
 
+
+  override def visitParenExpr(ctx: ParenExprContext): Klass = {
+    visit(ctx)
+  }
 
   override def visitIntLiteral(ctx: IntLiteralContext): Klass = {
     visitChildren(ctx)
@@ -45,6 +49,48 @@ class TypeCheckWalker(classes:ClassMap, scopes:ParseTreeProperty[Scope]) extends
     }
   }
 
+  override def visitType(ctx: TypeContext): Klass = {
+    val id = Option(ctx.ID())
+    id match {
+      case Some(i) =>
+        classes.get(i.getText) match {
+          case Some(klass) => klass
+          case None =>null
+      }
+      case None =>
+        val contextName = ctx.getText
+        classes.get(contextName) match {
+          case Some(klass) => klass
+          case None =>throw Errors.unexpectedContext()
+        }
+    }
+  }
+
+  override def visitVarDefinition(ctx: VarDefinitionContext): Klass = {
+    currentScope match {
+      case Some(rawScope)=>
+        rawScope match {
+          case method:Method =>
+            val varToDefRaw: Symbole = method.findSymbolDeeply(ctx.ID().getText).get
+            varToDefRaw match {
+              case varToDef:VariableSymbol =>
+                val expressionType = Option(visit(ctx.expr()))
+                expressionType match {
+                  case None => throw Errors.typeNotFound(ctx.expr().getText)
+                  case Some(expType:Klass) =>
+                    if(varToDef.typee.name == expType.name)
+                      expType
+                    else
+                      throw Errors.typeMismatch(varToDef.typee.name, expType.name)
+                }
+              case _=> throw Errors.invalidOpOnSymbolType(varToDefRaw)
+            }
+          case _=> throw Errors.unExpectedScope(rawScope)
+        }
+      case None=> throw Errors.noScopeFound()
+    }
+  }
+
   override def visitConstructorCall(ctx: ConstructorCallContext): Klass = {
     val className = ctx.ID().getText
     classes.get(className) match {
@@ -52,6 +98,8 @@ class TypeCheckWalker(classes:ClassMap, scopes:ParseTreeProperty[Scope]) extends
       case None => throw Errors.typeNotFound(className)
     }
   }
+
+  override def visitMethodDecl(ctx: MethodDeclContext): Klass = super.visitMethodDecl(ctx)
 
   override def visitThisCall(ctx: ThisCallContext): Klass = {
     visitChildren(ctx)
@@ -66,12 +114,46 @@ class TypeCheckWalker(classes:ClassMap, scopes:ParseTreeProperty[Scope]) extends
     }
   }
 
+  override def visitGreaterThanExpr(ctx: GreaterThanExprContext): Klass = super.visitGreaterThanExpr(ctx)
+
+  override def visitLessThanExpr(ctx: LessThanExprContext): Klass = super.visitLessThanExpr(ctx)
+
+  override def visitNotExpr(ctx: NotExprContext): Klass = super.visitNotExpr(ctx)
+
+  override def visitSubtractExpression(ctx: SubtractExpressionContext): Klass = super.visitSubtractExpression(ctx)
+
+  override def visitPlusExpression(ctx: PlusExpressionContext): Klass = super.visitPlusExpression(ctx)
+
+  override def visitMultiplyExpression(ctx: MultiplyExpressionContext): Klass = super.visitMultiplyExpression(ctx)
+
+  override def visitAndExpr(ctx: AndExprContext): Klass = super.visitAndExpr(ctx)
+
   override def visitBooleanLit(ctx: BooleanLitContext): Klass = {
     visitChildren(ctx)
     classes(BOOLEAN)
   }
 
-  private def stepInScope(ctx:ParserRuleContext): Unit = {
+
+  override def visitMainClass(ctx: MainClassContext): Klass = {
+    starClassScope(ctx)
+    null
+  }
+
+  override def visitBaseClass(ctx: BaseClassContext): Klass = {
+    starClassScope(ctx)
+    null
+  }
+
+  override def visitChildClass(ctx: ChildClassContext): Klass = {
+    starClassScope(ctx)
+    null
+  }
+
+  override def visitArrLenExpression(ctx: ArrLenExpressionContext): Klass = super.visitArrLenExpression(ctx)
+
+  override def visitArrayAccessExpression(ctx: ArrayAccessExpressionContext): Klass = super.visitArrayAccessExpression(ctx)
+
+  private def starClassScope(ctx:ParserRuleContext): Unit = {
     val searchScope = Option(scopes.get(ctx))
     searchScope match {
       case None => throw Errors.noScopeFound()
